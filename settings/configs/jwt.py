a38b513f89eb7c2a1eff14240a71bef7f0db6732
zucktang <zucktang@gmail.com>
2024-10-20 13:28:08 +0700

    remove unused code

--- settings/configs/jwt.py
+++ settings/configs/jwt.py
@@ -5,7 +5,6 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'TOKEN_OBTAIN_SERIALIZER': "FavLinks.services.auth.serializers.MyTokenObtainPairSerializer",
     "ALGORITHM": "HS256",
-    # "SIGNING_KEY": SECRET_KEY,
 }
 
 
